4b8ea77fc2e069a157d2e6303af40eac980cdb74
Philip Wahlbom <philip@wahlbom.net>
2022-02-05 17:43:00 -0500

    Update Program.cs

--- C-shpCreatingMethods/Program.cs
+++ C-shpCreatingMethods/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace C_shpCreatingMethods
 {
+    public class MathFunction {
+
+        public int AddNumbers(params int[] numbers) {
+
+            var intTotal = 0;
+
+            foreach (var number in numbers)
+            {
+                intTotal = intTotal + number;
+            }
+
+            return intTotal;
+        } 
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var Sum1 = new MathFunction();
+            var MySum = Sum1.AddNumbers(1,2,3,4,5);
+            Console.WriteLine(MySum);
+            Console.ReadLine(); 
+
         }
     }
 }
